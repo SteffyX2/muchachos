d57a1b66346fe87c9dabcb29a6ffa30693deef18
SteffyX2 <28824635+SteffyX2@users.noreply.github.com>
2019-10-14 23:44:49 +0200

    Update fiksni.js

--- js/fiksni.js
+++ js/fiksni.js
@@ -1,19 +1,19 @@
 var message = new Array();
-message[0] =  "  okupi Fiksni";
-message[1] =  "  xkupi Fiksni";
-message[2] =  "  okupi Fiksni";
-message[3] =  "  xkupi Fiksni";
-message[4] =  "  okupi Fiksni";
-message[5] =  "  xkupi Fiksni";
-message[6] =  "  okupi Fiksni";
-message[7] =  "  xkupi Fiksni";
-message[8] =  "  okupi Fiksni";
-message[9] =  "  xkupi Fiksni";
-message[10] = "  okupi Fiksni";
-message[11] = "  xkupi Fiksni";
-message[12] = "  okupi Fiksni";
-message[13] = "  xkupi Fiksni";
-message[14] = "  okupi Fiksni";
+message[0] =  "okupi Fiksni";
+message[1] =  "xkupi Fiksni";
+message[2] =  "okupi Fiksni";
+message[3] =  "xkupi Fiksni";
+message[4] =  "okupi Fiksni";
+message[5] =  "xkupi Fiksni";
+message[6] =  "okupi Fiksni";
+message[7] =  "xkupi Fiksni";
+message[8] =  "okupi Fiksni";
+message[9] =  "xkupi Fiksni";
+message[10] = "okupi Fiksni";
+message[11] = "xkupi Fiksni";
+message[12] = "okupi Fiksni";
+message[13] = "xkupi Fiksni";
+message[14] = "okupi Fiksni";
 message[15] = "21000";
 var reps = 2;
 var speed = 50; var p = message.length;
@@ -30,21 +30,21 @@ A();
 function A() {
 s++;
 if (s > 15) { s = 1;}
-if (s == 1)  { document.title = '[P] '+T+' '; }
-if (s == 2)  { document.title = '[x] '+T+' '; }
-if (s == 3)  { document.title = '[P]  '+T+' '; }
-if (s == 4)  { document.title = '[x]  '+T+' '; }
-if (s == 5)  { document.title = '[P] '+T+' '; }
-if (s == 6)  { document.title = '[x] '+T+' '; }
-if (s == 7)  { document.title = '[P]  '+T+' '; }
-if (s == 8)  { document.title = '[x]  '+T+' '; }
-if (s == 9)  { document.title = '[P] '+T+' '; }
-if (s == 10) { document.title = '[x] '+T+' '; }
-if (s == 11) { document.title = '[P]  '+T+' '; }
-if (s == 12) { document.title = '[x]  '+T+' '; }
-if (s == 13) { document.title = '[P] '+T+' '; }
-if (s == 14) { document.title = '[x] '+T+' '; }
-if (s == 15) { document.title = '[P]  '+T+' '; }
+if (s == 1)  { document.title = '[P]'+T+' '; }
+if (s == 2)  { document.title = '[x]'+T+' '; }
+if (s == 3)  { document.title = '[P]'+T+' '; }
+if (s == 4)  { document.title = '[x]'+T+' '; }
+if (s == 5)  { document.title = '[P]'+T+' '; }
+if (s == 6)  { document.title = '[x]'+T+' '; }
+if (s == 7)  { document.title = '[P]'+T+' '; }
+if (s == 8)  { document.title = '[x]'+T+' '; }
+if (s == 9)  { document.title = '[P]'+T+' '; }
+if (s == 10) { document.title = '[x]'+T+' '; }
+if (s == 11) { document.title = '[P]'+T+' '; }
+if (s == 12) { document.title = '[x]'+T+' '; }
+if (s == 13) { document.title = '[P]'+T+' '; }
+if (s == 14) { document.title = '[x]'+T+' '; }
+if (s == 15) { document.title = '[P]'+T+' '; }
 if (C < (15 * reps)) {
 sT = setTimeout("A()", speed);
 C++;
